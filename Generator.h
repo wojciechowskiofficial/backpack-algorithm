ed210a4a457391eae0fcb953d4c2029ecbb59a47
ADAM WOJCIECHOWSKI <adi61044@gmail.com>
2019-06-01 20:04:58 +0200

    Generator class added

---
+++ Generator.h
@@ -0,0 +1,5 @@
+class Generator {
+	public:
+		void gen(int quantity, int max_value, int max_weight);
+		Item * gen_arr;
+};
--- main.cpp
+++ main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 
+#include "Item.h"
+#include "Generator.h"
 int main() {
 	std::cout << "hello knapsacks!!!" << std::endl;
 	return 0;
